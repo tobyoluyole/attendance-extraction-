f52bc23cf80f1122eb92e512b254ffd71f4ad8ea
OLUYOLE OLUWATOBI <tobioluyole@gmail.com>
2023-04-15 12:48:51 +0100

    able to send attendance report via email

--- run.py
+++ run.py
@@ -48,41 +48,49 @@ for row in range(2, sheet.max_row + 1):
 # Save the Attendance excel file
 wb.save('Attendance.xlsx')
 
-'''import smtplib
-from datetime import date
+
+
+import os
+import smtplib
+import datetime 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
-# set up email addresses and password
-sender_email = 'bluetoothaddress@gmail.com'
-sender_password = 'blueaddress'
-receiver_email = 'tobioluyole@gmail.com','robertoszn9@gmail.com'
+# Set up the email addresses and password
+sender_email = "isktobi@gmail.com"
+sender_password = "gusunnkswqqswdbn"
+receiver_email = "tobioluyole@gmail.com, oluyolevincent@gmail.com"
 
-# set up message
-msg = MIMEMultipart()
-msg['From'] = sender_email
-msg['To'] = receiver_email
-msg['Subject'] = 'Attendance for today'
+# Create a message object instance
+message = MIMEMultipart()
 
-# add body with current date
-today = date.today().strftime("%B %d, %Y")
-body = f"Please find attached to this mail the attendance for {today}"
-msg.attach(MIMEText(body, 'plain'))
+# Set the message parameters
+message['From'] = sender_email
+message['To'] = receiver_email
+message['Subject'] = "Attendance Report for Today"
 
-# add attachment
-filename = 'Attendance.xlsx'
-with open(filename, 'rb') as f:
-    attach = MIMEApplication(f.read(), _subtype="xlsx")
-    attach.add_header('Content-Disposition', 'attachment', filename=filename)
-    msg.attach(attach)
+# Add the body of the email (including today's date)
+today_date = datetime.date.today().strftime('%Y-%m-%d')
+message.attach(MIMEText("Please find attached the attendance report for today ({})".format(today_date)))
 
-# send email
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.starttls()
-    smtp.login(sender_email, sender_password)
-    smtp.send_message(msg)
-    print('Email sent successfully.')'''
+# Attach the Excel file
+filename = "Attendance.xlsx"
+with open(filename, "rb") as attachment:
+    part = MIMEApplication(
+        attachment.read(),
+        Name=os.path.basename(filename)
+    )
+    part['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(filename))
+    message.attach(part)
+
+# Connect to the SMTP server and send the email
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.starttls()
+    server.login(sender_email, sender_password)
+    text = message.as_string()
+    server.sendmail(sender_email, receiver_email.split(','), text)
+
 
 
 
